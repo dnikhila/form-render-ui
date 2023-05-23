12c239312504418ddd228c3354434c811dee372a
nikhila <nikhila.darisa@opiesoftware.com>
2023-05-23 12:52:07 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-import MyCounter from './components/App'
-
-export { MyCounter }
+export { default as RenderForm } from "./render";
---
+++ src/render/Render.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button } from "../components";
+import RadioButtonsGroup from "../components/Radio";
+
+
+interface FormFields{
+    component:{
+        type:string;
+        value:string;
+        options:any;
+    }
+}
+
+function Factory(props:FormFields) {
+    switch (props.component.type) {
+      case "Button":
+        return <Button label={""}/>;
+      case "Radio":
+        return <RadioButtonsGroup/>;
+      default:
+        return <div>Reload...</div>;
+    }
+  }
+
+function RenderForm(formFields:FormFields[]){
+    return(
+        <div>
+            {formFields.map((field)=>{
+                return(
+                    <Factory component={field.component}  />
+                )
+            })}
+        </div>
+    )
+}
+export default RenderForm
